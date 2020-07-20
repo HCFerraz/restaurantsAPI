c41f5a5ffae3e9fa6dede0f0be56543698c02612
HCFerraz <henriquecabralferraz@gmail.com>
2020-07-19 21:45:20 -0300

    axios

--- api/axios.js
+++ api/axios.js
@@ -3,9 +3,8 @@ const axios = require('../node_modules/axios').default;
 const TOKEN1 = 'edaf0187-0e7b-456e-a177-cd05a95bf42a';
 const newRestaurant = 'Japa Sushi Express'
 const id = '5f14c02c76c8cf4ea4a3ed02'
-// data: {name: 'New Restaurant'}
 
-// axios.post('http://localhost:3000/signUp', {email: 'lasanha@email.com'}).then(response => console.log(response))
+axios.post('http://localhost:3000/signUp', {email: 'lasanha@email.com'}).then(response => console.log(response))
 
 axios.get(`http://localhost:3000/restaurant/name/${newRestaurant}`)
     .then(response => console.log(response.data))
