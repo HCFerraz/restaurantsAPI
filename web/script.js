f5dfc43aeb6f5026a1595abe009a0a662f5245c2
HCFerraz <henriquecabralferraz@gmail.com>
2020-07-30 19:37:16 -0300

    Finishing some features

--- web/script.js
+++ web/script.js
@@ -18,6 +18,7 @@ function auth() {
     const receiveEmail = document.getElementById('restaurant').value
     signUser(receiveEmail)
 }
+
 // ------------------------------ GETS
 const callForRestName = async (restaurant) => {
     try {
@@ -55,16 +56,34 @@ const returnRestaurantByCuisine = (restaurant) => {
 const returnAll = () => {
     axios.get(`http://localhost:3000/restaurants`).then((response => console.log(response))).catch(error => console.log(error))
 }
-// ------------------------------ POSTS
-const signUser = async (auth) => {
-    try {
-        console.log(auth)
-        const newAuth = await axios.post('http://localhost:3000/signUp', {email: auth})
-        return newAuth.data
-    } catch (error) {
-        console.log(error)
-    }
+
+// ------------------------------ SIGNUP
+const signUser = (auth) => {
+    axios.post('http://localhost:3000/signUp', { email: auth }).then(response => alert(`${response.data.message}: ${response.data.token}`)).catch(error => alert(`Insert a valid email!\n${error}`))
 }
+
+// ------------------------------ POSTS
+const postNewRestaurant = (restaurantName, restaurantCuisine, restaurantBorough, TOKEN) => {
+    axios.post('http://localhost:3000/restaurants/add', { name: restaurantName, cuisine: restaurantCuisine, borough: restaurantBorough }, { headers: { 'Token': TOKEN } })
+        .then((response) => console.log(response.data))
+        .catch(error => console.log(error))
+}
+
 // ------------------------------ PUTS
+const updateRestaurant = (restaurantID, restaurantCuisine, TOKEN) => {
+    axios.put(`http://localhost:3000/updateRestaurant/id/${restaurantID}`, { cuisine: restaurantCuisine }, { headers: { 'Token': TOKEN } })
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+}
 
 // ------------------------------ DELETES
+const deleteOneRest = (restaurantName, TOKEN) => {
+    axios.delete(`http://localhost:3000/removeOneRestaurant`, { headers: { 'Token': TOKEN }, data: { name: restaurantName } })
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+}
+const deleteManyRests = (restaurantName, TOKEN) => {
+    axios.delete(`http://localhost:3000/removeManyRestaurants`, { headers: { 'Token': TOKEN }, data: { name: restaurantName } })
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+}
